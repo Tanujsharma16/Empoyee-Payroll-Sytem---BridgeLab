7d55d54636119f355a2260112b6e720c4a81fbeb
Tanuj Sharma <tanuj@example.com>
2026-02-20 11:20:22 +0530

    Changes

--- server.js
+++ server.js
@@ -11,17 +11,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
+// HOME PAGE
 app.get('/', async (req, res) => {
     const employees = await fileHandler.read();
     res.render('index', { employees });
 });
 
+// ADD PAGE
 app.get('/add', (req, res) => {
     res.render('add');
 });
 
+// ADD EMPLOYEE
 app.post('/add', async (req, res) => {
-    const { name, position, salary, gender, DateOfJoining } = req.body;
+    const { name, position, salary, gender, DateOfJoining, avatar } = req.body;
 
     const employees = await fileHandler.read();
 
@@ -31,26 +34,37 @@ app.post('/add', async (req, res) => {
         position,
         gender: gender || "Not set",
         DateOfJoining,
-        salary: Number(salary)
+        salary: Number(salary) || 0,
+        avatar: avatar || 1   
     });
 
     await fileHandler.write(employees);
     res.redirect('/');
 });
 
+// EDIT PAGE
 app.get('/edit/:id', async (req, res) => {
     const employees = await fileHandler.read();
     const employee = employees.find(e => e.id == req.params.id);
     res.render('edit', { employee });
 });
 
+// UPDATE EMPLOYEE
 app.post('/edit/:id', async (req, res) => {
-    const { name, position, salary, gender, DateOfJoining } = req.body;
+    const { name, position, salary, gender, DateOfJoining, avatar } = req.body;
     let employees = await fileHandler.read();
 
     employees = employees.map(emp =>
         emp.id == req.params.id
-            ? { ...emp, name, position, gender, salary: Number(salary), DateOfJoining }
+            ? {
+                ...emp,
+                name,
+                position,
+                gender,
+                salary: Number(salary) || 0,
+                DateOfJoining,
+                avatar: avatar || emp.avatar || 1  
+            }
             : emp
     );
 
@@ -58,6 +72,7 @@ app.post('/edit/:id', async (req, res) => {
     res.redirect('/');
 });
 
+// DELETE EMPLOYEE
 app.get('/delete/:id', async (req, res) => {
     let employees = await fileHandler.read();
     employees = employees.filter(emp => emp.id != req.params.id);
